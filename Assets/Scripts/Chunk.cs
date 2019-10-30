a604c43818fb3c5d39e6428bca785c53f6bbac95
TheLumberJohn <tataich@gmail.com>
2019-10-30 18:43:19 +0300

    Basic shader and lighting implemented

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -19,6 +19,7 @@ public class Chunk
     List<int> transparentTriangles = new List<int>();
     Material[] materials = new Material[2];
     List<Vector2> uvs = new List<Vector2>();
+    List<Color> colors = new List<Color>();
 
     public Vector3 position;
 
@@ -50,10 +51,14 @@ public class Chunk
         meshFilter = chunkObject.AddComponent<MeshFilter>();
         meshRenderer = chunkObject.AddComponent<MeshRenderer>();
 
+        /*
         materials[0] = world.material;
         materials[1] = world.transparentMaterial;
+        
         meshRenderer.materials = materials;
+        */
 
+        meshRenderer.material = world.material;
 
         chunkObject.transform.SetParent(world.transform);
         chunkObject.transform.position = new Vector3(coord.x * VoxelData.ChunkWidth, 0f, coord.z * VoxelData.ChunkWidth);
@@ -127,6 +132,7 @@ public class Chunk
         triangles.Clear();
         transparentTriangles.Clear();
         uvs.Clear();
+        colors.Clear();
     }
 
     public bool isActive
@@ -239,16 +245,42 @@ public class Chunk
 
                 AddTexture(world.blockTypes[blockID].GetTextureID(p));
 
-                // Based on pattern found in between short and long versions of VoxelData.voxelTris
-                if (!isTransparent)
+                float lightLevel;
+
+                int yPos = (int)pos.y + 1;
+                bool inShade = false;
+                while(yPos < VoxelData.ChunkHeight)
                 {
-                    triangles.Add(vertexIndex);
+                    if(voxelMap[(int)pos.x, yPos, (int)pos.z] != 0)
+                    {
+                        inShade = true;
+                        break;
+                    }
+
+                    yPos++;
+                }
+
+                if (inShade)
+                    lightLevel = 0.5f;
+                else
+                    lightLevel = 0f;
+
+                colors.Add(new Color(0, 0, 0, lightLevel));
+                colors.Add(new Color(0, 0, 0, lightLevel));
+                colors.Add(new Color(0, 0, 0, lightLevel));
+                colors.Add(new Color(0, 0, 0, lightLevel));
+
+                // Triangles add sequence is based on pattern found in between short and long versions of VoxelData.voxelTris
+
+                /*if (!isTransparent)
+                {*/
+                triangles.Add(vertexIndex);
                     triangles.Add(vertexIndex + 1);
                     triangles.Add(vertexIndex + 2);
                     triangles.Add(vertexIndex + 2);
                     triangles.Add(vertexIndex + 1);
                     triangles.Add(vertexIndex + 3);
-                }
+                /*}
                 else
                 {
                     transparentTriangles.Add(vertexIndex);
@@ -257,7 +289,7 @@ public class Chunk
                     transparentTriangles.Add(vertexIndex + 2);
                     transparentTriangles.Add(vertexIndex + 1);
                     transparentTriangles.Add(vertexIndex + 3);
-                }
+                }*/
 
 
                 vertexIndex += 4;
@@ -270,11 +302,17 @@ public class Chunk
         Mesh mesh = new Mesh();
         mesh.vertices = vertices.ToArray();
 
+        /*
         mesh.subMeshCount = 2;
+        
         mesh.SetTriangles(triangles.ToArray(), 0);
         mesh.SetTriangles(transparentTriangles.ToArray(), 1);
+        */
+        mesh.triangles = triangles.ToArray();
 
         mesh.uv = uvs.ToArray();
+
+        mesh.colors = colors.ToArray();
 
         mesh.RecalculateNormals();
 
--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -7,6 +7,11 @@ public class World : MonoBehaviour
     public int seed;
     public BiomeAttributes biome;
 
+    [Range(0.95f, 0f)]
+    public float globalLightLevel;
+    public Color day;
+    public Color night;
+
     public Transform player;
     public Vector3 spawnPosition;
 
@@ -40,7 +45,7 @@ public class World : MonoBehaviour
     {
         Random.InitState(seed);
 
-        spawnPosition = new Vector3((VoxelData.WorldSizeInChunks * VoxelData.ChunkWidth) / 2f, VoxelData.ChunkHeight - 10f, (VoxelData.WorldSizeInChunks * VoxelData.ChunkWidth) / 2f);
+        spawnPosition = new Vector3((VoxelData.WorldSizeInChunks * VoxelData.ChunkWidth) / 2f, VoxelData.ChunkHeight - 20f, (VoxelData.WorldSizeInChunks * VoxelData.ChunkWidth) / 2f);
         GenerateWorld();
         playerLastChunkCoord = GetChunkCoordFromVector3(player.position);
     }
@@ -48,6 +53,9 @@ public class World : MonoBehaviour
     private void Update()
     {
         playerChunkCoord = GetChunkCoordFromVector3(player.position);
+
+        Shader.SetGlobalFloat("GlobalLightLevel", globalLightLevel);
+        Camera.main.backgroundColor = Color.Lerp(day, night, globalLightLevel);
 
         if (!playerChunkCoord.Equals(playerLastChunkCoord))
             CheckViewDistance();
